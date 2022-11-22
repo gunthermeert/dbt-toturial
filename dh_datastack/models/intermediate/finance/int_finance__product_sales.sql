3b399efd6d7d30375a089529077a94b941804dc4
GuntherMeert <gunthermeert@hotmail.com>
2022-11-22 13:52:51 +0100

    int_finance__daily_sales_per_product.sql

---
+++ dh_datastack/models/intermediate/finance/int_finance__product_sales.sql
@@ -0,0 +1,34 @@
+with
+cte_dates as
+(
+    select * from {{ ref('all_dates') }}
+),
+cte_customers as
+(
+    select * from {{ ref('stg_dh_shop__customers') }}
+),
+cte_products as
+(
+    select * from {{ ref('stg_dh_shop__products') }}
+),
+cte_orders as
+(
+    select * from {{ ref('stg_dh_shop__orders') }}
+),
+cte_orderlines as
+(
+    select * from {{ ref('stg_dh_shop__orderlines') }}
+)
+, cte_final as (
+    select col.orderline_id, co.order_id, cp.product_id, cc.customer_id, co.order_date, cp.product, col.amount_sold as quantity, cp.market_price as price
+    from cte_orderlines col
+    inner join cte_orders co
+    on col.order_id = co.order_id
+    inner join cte_products cp
+    on col.product_id = cp.product_id
+    inner join cte_customers cc
+    on co.customer_id = cc.customer_id
+)
+select *
+from cte_final
+
