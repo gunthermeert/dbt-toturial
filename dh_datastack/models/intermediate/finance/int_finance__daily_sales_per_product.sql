5aceae45aa365f025de2721bafcfa2bab922adba
GuntherMeert <gunthermeert@hotmail.com>
2022-11-22 13:15:56 +0100

    int_finance__daily_sales_per_product.sql

---
+++ dh_datastack/models/intermediate/finance/int_finance__daily_sales_per_product.sql
@@ -0,0 +1,26 @@
+with
+cte_dates as
+(
+    select * from {{ ref('all_dates') }}
+),
+cte_customers as
+(
+    select * from {{ ref('stg_dh_shop__customers') }}
+),
+cte_products as
+(
+    select * from {{ ref('stg_dh_shop__products') }}
+),
+cte_orders as
+(
+    select * from {{ ref('stg_dh_shop__orders') }}
+),
+cte_orderlines as
+(
+    select * from {{ ref('stg_dh_shop_orderlines') }}
+)
+select *
+from cte_dates cd
+left join cte_orders co
+on cd.date_day = co.order_date;
+
