d4db9acf8872aca36f0836be30c4cd6762887d2b
GuntherMeert <gunthermeert@hotmail.com>
2022-12-06 10:11:35 +0100

    refactor source names finance

--- dh_datastack_finance/models/staging/dh_datastack_mdm/stg_dh_datastack_mdm__customers.sql
+++ dh_datastack_finance/models/staging/dh_datastack_mdm/stg_dh_datastack_mdm__customers.sql
@@ -1,5 +1,5 @@
 with source as (
-    select * from {{ source('dh_datastack_mdm','STG_DH_DATASTACK_MDM__CUSTOMERS') }}
+    select * from {{ source('dh_datastack_mdm','STG_DH_SHOP__CUSTOMERS') }}
 ),
 customers as (
     select
