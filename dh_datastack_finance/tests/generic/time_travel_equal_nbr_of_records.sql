21358868fafd7c4d2556d89a2eefa92b1dd5457b
GuntherMeert <gunthermeert@hotmail.com>
2022-12-09 11:28:54 +0100

    add time_travel_test

--- dh_datastack_finance/tests/generic/time_travel_equal_nbr_of_records.sql
+++ dh_datastack_finance/tests/generic/time_travel_equal_nbr_of_records.sql
@@ -1,14 +1,14 @@
-{% test time_travel_equal_nbr_of_records(model, column_name, count, period, expression, condition, quote=False) %}
+{% test time_travel_equal_nbr_of_records(model, column_name, count, period, expression, where_clause, quote=False) %}
 
 with time_travel as (
     select count(*) nbr_records_time_travel
     from {{ model }}
     at(timestamp => dateadd({{ period }}, -{{ count }}, current_timestamp()))
-    where {{ condition }}
+    where {{ where_clause }}
 ), current_data as (
     select count(*) as nbr_records_current_data
     from {{ model }}
-    where {{ condition }}
+    where {{ where_clause }}
 ), difference as (
 select abs((select nbr_records_time_travel from  time_travel)- (select nbr_records_current_data from  current_data)) as nbr_different
 )
