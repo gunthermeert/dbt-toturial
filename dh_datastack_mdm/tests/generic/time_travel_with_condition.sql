c90d712ec87efb05c9b815d71f56054c0a605d02
GuntherMeert <gunthermeert@hotmail.com>
2023-02-02 15:18:55 +0100

    add time travel compare column values test

--- dh_datastack_mdm/tests/generic/time_travel_with_condition.sql
+++ dh_datastack_mdm/tests/generic/time_travel_with_condition.sql
@@ -11,6 +11,9 @@ parameters:
     condition: string, extra where statements starting with and because of comparing columns first
 */
 
+{{ config(
+  enabled=true
+) }}
 
 with cte_time_travel as (
 select *, 'time_travel' as source
