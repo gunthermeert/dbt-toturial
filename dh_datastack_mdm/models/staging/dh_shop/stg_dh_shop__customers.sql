4b3ec969a76d433ca319eb0e48e3b83ae4eec8d9
GuntherMeert <gunthermeert@hotmail.com>
2023-02-02 08:51:25 +0100

    make stg_dh_shop__customers incremental

--- dh_datastack_mdm/models/staging/dh_shop/stg_dh_shop__customers.sql
+++ dh_datastack_mdm/models/staging/dh_shop/stg_dh_shop__customers.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='incremental'
+        unique_key='id'
+    )
+}}
 with source as (
     select * from {{ source('dh_shop','customers') }}
 ),
@@ -14,7 +20,14 @@ customers as (
         phone1 as first_phone,
         phone2 as second_phone,
         email,
-        current_timestamp() as loaded_at
+        current_timestamp() as loaded_at,
+        update_ts
     from source
 )
 select * from customers
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where update_ts > (select max(update_ts) from {{ this }})
+
+{% endif %}
