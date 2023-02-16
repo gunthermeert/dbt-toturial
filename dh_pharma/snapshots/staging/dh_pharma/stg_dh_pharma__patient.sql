51eb1b974f50d47c18b37da764338dd59507c764
GuntherMeert <gunthermeert@hotmail.com>
2023-02-16 14:29:16 +0100

    att patient.csv

---
+++ dh_pharma/snapshots/staging/dh_pharma/stg_dh_pharma__patient.sql
@@ -0,0 +1,17 @@
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+      target_database='DH_PHARMA',
+      target_schema='DH_PHARMA_STG',
+      unique_key='id',
+
+      strategy='check',
+      check_cols='all',
+      invalidate_hard_deletes=True,
+    )
+}}
+
+select * from {{ source('dh_pharma', 'patient') }}
+
+{% endsnapshot %}
