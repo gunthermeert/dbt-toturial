1da6261cf3d6120da032cffbd8b805d7ffbc24f5
GuntherMeert <gunthermeert@hotmail.com>
2023-02-16 16:26:34 +0100

    dim_patients

---
+++ dh_pharma/models/datamarts/dim_patients.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+with patients as (
+select dbt_scd_id as patient_sk
+, patient_id
+, age
+, street_name
+, house_number
+, city
+, district
+, state
+, country
+, insert_ts
+, update_ts
+, valid_from
+, valid_to
+, current_flg
+from {{ ref('int_dh_pharma__patients') }}
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where update_ts > (select max(update_ts) from {{ this }})
+
+{% endif %}
+) select * from patients
--- dh_pharma/models/intermediate/int_dh_pharma__patients.sql
+++ dh_pharma/models/intermediate/int_dh_pharma__patients.sql
@@ -1,5 +1,12 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 with patients as (
-select patient_id
+select dbt_scd_id as patient_sk
+, patient_id
 , age
 , street_name
 , house_number
@@ -7,5 +14,16 @@ select patient_id
 , district
 , state
 , country
+, dbt_updated_at as insert_ts
+, dbt_updated_at as update_ts
+, dbt_valid_from as valid_from
+, coalesce(dbt_valid_to, '9999-12-31')::timestamp as valid_to
+, case when dbt_valid_to is null then 1 else 0 end as current_flg
 from {{ ref('snap_patients') }}
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where dbt_updated_at > (select max(dbt_updated_at) from {{ this }})
+
+{% endif %}
 ) select * from patients
