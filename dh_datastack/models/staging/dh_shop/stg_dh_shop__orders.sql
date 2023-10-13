92f5c872df962c9dbbe9884288ad2d9779973934
Gunther Meert <gunthermeert@hotmail.com>
2023-10-13 08:34:53 +0000

    test

--- dh_datastack/models/staging/dh_shop/stg_dh_shop__orders.sql
+++ dh_datastack/models/staging/dh_shop/stg_dh_shop__orders.sql
@@ -1,11 +1,1 @@
-with source as (
-    select * from {{ source('dh_shop','orders') }}
-),
-orders as (
-    select
-    order_id,
-    customer_id,
-    order_date
-    from source
-)
-select * from orders
+select 1 as nbr
